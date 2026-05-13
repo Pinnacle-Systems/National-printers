9b8b2f8457a1a09ffe4c3554225fc7228e24d028
Manoj bharathi <manojbharathi00@gmail.com>
2026-05-13 11:10:46 +0530

    port change

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const apiTarget = env.VITE_API_PROXY_TARGET ?? "http://localhost:8080";
+  const apiTarget = env.VITE_API_PROXY_TARGET ?? "http://localhost:6900";
 
   return {
     plugins: [react({ include: /\.(js|jsx|ts|tsx)$/ })],
